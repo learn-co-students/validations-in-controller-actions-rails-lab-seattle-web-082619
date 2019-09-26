ef8731a20ba7321c7a734c3f426f58d9cc68d0c2
ianhunterharold <ianhunterharold@gmail.com>
2019-09-26 12:23:25 -0700

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,15 +1,19 @@
 class AuthorsController < ApplicationController
   def show
-    @author = Author.find(params[:id])
+    @author = author_find_param
   end
 
   def new
   end
 
   def create
-    @author = Author.create!(author_params)
-
+    @author = Author.create(author_params)
+      if @author.valid? 
+      @author.save
     redirect_to author_path(@author)
+    else 
+      render :new 
+    end 
   end
 
   private
@@ -17,4 +21,9 @@ class AuthorsController < ApplicationController
   def author_params
     params.permit(:email, :name)
   end
+
+  def author_find_param
+    Author.find(params[:id])
+  end 
+  
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,41 @@
+require 'pry'
 class PostsController < ApplicationController
   before_action :set_post!, only: [:show, :edit, :update]
 
-  def show
+  def new 
+    # binding.pry
+    @post = Post.new
+  end 
+
+  def show 
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.valid? 
+      @post.save 
+      redirect_to post_path(@post)
+    else 
+      redirect_to post_path(@post)
+    end 
+  end 
 
   def edit
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+  
+    if @post.update(post_params)
+        redirect_to post_path(@post)
+    else 
+      render :edit
+    end 
   end
 
   private
 
   def post_params
-    params.permit(:category, :content, :title)
+    params.permit(:title, :category, :content)
   end
 
   def set_post!
